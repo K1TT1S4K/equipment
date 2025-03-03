6e05a3a950d4aeabfdac33dbd8ca91fa525ef1f3
K1TT1S4K <150118251+K1TT1S4K@users.noreply.github.com>
2025-03-03 18:27:59 +0700

    champ_switch_welcome_to_login

--- routes/web.php
+++ routes/web.php
@@ -3,9 +3,12 @@
 use Illuminate\Support\Facades\Route;
 use Livewire\Volt\Volt;
 
+// Route::get('/', function () {
+//     return view('welcome');
+// })->name('home');
 Route::get('/', function () {
-    return view('welcome');
-})->name('home');
+    return view('livewire.auth.login');
+})->name('login');
 
 Route::view('dashboard', 'dashboard')
     ->middleware(['auth', 'verified'])
